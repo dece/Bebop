4192c2a84db503d42e317567b97dfc1436e8b573
dece <shgck@pistache.land>
2021-05-29 01:15:12 +0200

    welcome: tell user how to change the home page

--- bebop/welcome.py
+++ bebop/welcome.py
@@ -5,10 +5,13 @@ WELCOME_PAGE = """\
 
 Hi! Welcome to the Bebop browser! 🚀🎶
 
-Press "?" to see the keybinds, commands and more, or visit the link below by \
-pressing its link ID (1). To start browsing right away, press "o", type an URL \
-and press Enter.
+Press "?" or type ":help" and enter to see the keybinds, commands and more, \
+or visit the link below by pressing its link ID (1). To start browsing \
+right away, press "o", type an URL and press Enter.
 
-=> bebop:help Help
-=> gemini://dece.space/dev/bebop.gmi Online documentation
+=> bebop:help Offline help page
+=> gemini://dece.space/dev/bebop.gmi Online Bebop home
+
+You can configure which page to show up when starting Bebop instead of this \
+one: set your home URL in the "home" key of your configuration file.
 """
