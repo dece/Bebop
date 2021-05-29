448ae873a00d3e57d5cd0ef1c1dcd0980c6b3d78
dece <shgck@pistache.land>
2021-05-29 15:24:11 +0200

    mime: ut

---
+++ bebop/tests/test_mime.py
@@ -0,0 +1,29 @@
+import unittest
+
+from ..mime import MimeType, DEFAULT_CHARSET, DEFAULT_MIME_TYPE
+
+
+class TestMime(unittest.TestCase):
+
+    def test_from_str(self):
+        self.assertIsNone(MimeType.from_str(""))
+        self.assertIsNone(MimeType.from_str("dumb"))
+        self.assertIsNone(MimeType.from_str("dumb;dumber"))
+        self.assertIsNone(MimeType.from_str("123456"))
+
+        mime = MimeType.from_str("a/b")
+        self.assertEqual(mime.main_type, "a")
+        self.assertEqual(mime.sub_type, "b")
+        self.assertEqual(mime.parameters, {})
+
+        mime = MimeType.from_str("text/gemini")
+        self.assertEqual(mime.main_type, "text")
+        self.assertEqual(mime.sub_type, "gemini")
+        self.assertEqual(mime.parameters, {})
+
+        mime = MimeType.from_str("text/gemini;lang=en")
+        self.assertEqual(mime.main_type, "text")
+        self.assertEqual(mime.sub_type, "gemini")
+        self.assertEqual(mime.parameters, {"lang": "en"})
+        mime = MimeType.from_str("text/gemini ;lang=en")
+        self.assertEqual(mime.parameters, {"lang": "en"})
