2ea8f607395bdda01ca92fa220b35683e0bbfb9e
dece <shgck@pistache.land>
2021-05-30 23:29:18 +0200

    welcome: minor changes

--- bebop/welcome.py
+++ bebop/welcome.py
@@ -3,7 +3,7 @@
 WELCOME_PAGE = """\
 # Bebop
 
-Hi! Welcome to the Bebop browser! 🚀🎶
+Welcome to the Bebop browser! 🚀
 
 Press "?" or type ":help" and enter to see the keybinds, commands and more, \
 or visit the link below by pressing its link ID (1). To start browsing \
@@ -14,4 +14,8 @@ right away, press "o", type an URL and press Enter.
 
 You can configure which page to show up when starting Bebop instead of this \
 one: set your home URL in the "home" key of your configuration file.
+
+New to Gemini? Check out this catalog of capsules to get you started:
+
+=> gemini://medusae.space/ medusae.space
 """
