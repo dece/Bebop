a2051e082da3eed9cc0934fe033ec737f85e7981
dece <shgck@pistache.land>
2021-06-28 02:13:38 +0200

    welcome: add geminiquickst.art link and `:q`

--- bebop/welcome.py
+++ bebop/welcome.py
@@ -13,17 +13,20 @@ WELCOME_PAGE = """\
 
 # Welcome to the Bebop browser! 🚀
 
-Press "?" or type ":help" and enter to see the keybinds, commands and more, \
-or visit the link below by pressing its link ID (1). To start browsing \
+Press "?" or type ":help" and press enter to see the keybinds, commands and \
+more, or visit the link below by pressing its link ID (1). To start browsing \
 right away, press "o", type an URL and press Enter.
 
 => bebop:help Offline help page
 => gemini://dece.space/dev/bebop.gmi Online Bebop home
 
+To quit Bebop, type `:q` and press enter.
+
 You can configure which page to show up when starting Bebop instead of this \
 one: set your home URL in the "home" key of your configuration file.
 
-New to Gemini? Check out this catalog of capsules to get you started:
+New to Gemini? Check out these capsules to get you started:
 
-=> gemini://medusae.space/ medusae.space
+=> gemini://geminiquickst.art Gemini Quickstart
+=> gemini://medusae.space/ The medusae.space catalog
 """
